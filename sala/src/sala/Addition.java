d77c796279fa4adad58f788c0e45a6f4b0026a94
Jameel <Jameel@DESKTOP-I4APKIL>
2022-02-03 08:57:59 -0800

    addition

---
+++ sala/src/sala/Addition.java
@@ -0,0 +1,12 @@
+package sala;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		 int a=20,b=30,c;
+		 c=a+b;
+		 System.out.println("addition of number is="+c);
+		 
+	}
+
+}
