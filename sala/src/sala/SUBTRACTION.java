4b9b8c4eeeddf4f7a665d011c8c3022394450641
Jameel <Jameel@DESKTOP-I4APKIL>
2022-02-03 09:07:05 -0800

    subtraction

---
+++ sala/src/sala/SUBTRACTION.java
@@ -0,0 +1,12 @@
+package sala;
+
+public class SUBTRACTION {
+
+	public static void main(String[] args) {
+		 int a=30,b=20,c;
+		 c=a-b;
+		 System.out.println("SUBTRCTION of number is="+c);
+		 
+	}
+
+}
